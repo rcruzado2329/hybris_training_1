bf9f665332b8b9b8bc1a7e9520f32f648595a726
Richar Cruzado <richard.cruzado23@gmail.com>
2022-12-21 19:32:28 -0500

    add module
    add files

---
+++ bin/modules/b2b-accelerator-addons/base-accelerator/yacceleratorbackoffice/gensrc/org/yacceleratorbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 4 ago. 2022 17:52:09                        ---
+ * ----------------------------------------------------------------
+ */
+package org.yacceleratorbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "yacceleratorbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
